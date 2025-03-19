7b7ff0f68c751ef9c5775fdf21594b225c2a843e
MOMAMMAD RANA <105457666+TOXIC-RANA@users.noreply.github.com>
2025-03-19 11:24:09 +0600

    Create server.js

---
+++ .env/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const gistRoutes = require("./routes/gistRoutes");
+
+const app = express();
+connectDB(); // MongoDB কানেক্ট করা
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", gistRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
